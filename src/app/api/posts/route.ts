d44c34cc2be677b4d79288789607f4c9cdcb4f2f
Dan McAtee <danielmcatee@me.com>
2023-07-27 11:22:03 +0200

    show dev deploys

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 
 export async function GET(req: Request) {
   console.log(req)
-  return NextResponse.json({ message: "Hello" })
+  return NextResponse.json({ message: "Hello from dev" })
 }
 
 export async function POST(req: Request) {
